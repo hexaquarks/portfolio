e2e6c5710387ad593c2e1b0f76ae1adb4b5da6ad
hexaquarks <seseriusca@gmail.com>
2022-04-13 23:55:18 -0400

    Removed unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cover from './components/cover/Cover';
 import About from './components/about/About';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
