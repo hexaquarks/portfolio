0a57f3552cdadd4d3591a239748b59abe9542e55
hexaquarks <seseriusca@gmail.com>
2022-04-23 00:32:30 -0400

    Changed to FontAwesome for Bottom Navbar

--- src/components/navbars/BottomNavbar.tsx
+++ src/components/navbars/BottomNavbar.tsx
@@ -2,18 +2,14 @@ import { useState } from 'react';
 import githubIcon from '../../assets/githubOrange.png'
 import linkedIn from '../../assets/linkedinIcon.png'
 import styles from './Navbars.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const BottomNavbar = () => {
     return (
         <div className={styles.bottomNavbarContainer}>
-            <img src={githubIcon} width="50px" height="50px"/>
-            {/* <div className={styles.icon}>
-                <img src={githubIcon} width="50px" height="50px"/>
-            </div> */}
-            <img src={linkedIn} width="50px" height="50px"/>
-            {/* <div className={styles.icon}>
-                <img src={linkedIn} width="50px" height="50px"/>
-            </div> */}
+            <FontAwesomeIcon icon={faGithub} className={styles.icon}/>
+            <FontAwesomeIcon icon={faLinkedin} className={styles.icon}/>
         </div>
     );
 }
