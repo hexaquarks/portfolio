9fe31a557d0994a033396e6d6a259f92b15eccbc
hexaquarks <seseriusca@gmail.com>
2022-05-01 23:10:13 -0400

    Started burger menu scrollable linking

--- src/components/navbars/TopNavbar.tsx
+++ src/components/navbars/TopNavbar.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import styles from './Navbars.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faCircleInfo, faFolderClosed, faIdCard } from '@fortawesome/free-solid-svg-icons';
 import SelectionItem from '../cover/SelectionItem';
-import { slide as Menu } from 'react-burger-menu';
+import { stack as Menu } from 'react-burger-menu';
+import Scroll, { Link }  from 'react-scroll'
 
 
 const TopNavbar = () => {
     const sections = ["Home", "About", "Projects", "Contact"];
-
+    const icons = [faHome, faCircleInfo, faFolderClosed, faIdCard];
+    const names = ["Home", "About", "Projects", "Contact"];
     const selectionItems = [
         {
             name: "Home",
@@ -44,11 +47,35 @@ const TopNavbar = () => {
                 </a>
             </div>
             <div className={styles.itemsRight}>
-                <Menu className={styles.bm_burger_menu}>
-                    <a id="home" className="menu-item">Home</a>
-                    <a id="about" className="menu-item" >About</a>
-                    <a id="contact" className="menu-item" >Contact</a>
-                    <a >Settings</a>
+                <Menu right>
+                        <a className="menu-item">
+                            <FontAwesomeIcon icon={icons[0]} className={styles.icon}/>
+                            {names[0]}
+                        </a>
+                        <a className="menu-item">
+                            <FontAwesomeIcon icon={icons[1]} className={styles.icon}/>
+                            {names[1]}
+                        </a>
+                        <a className="menu-item">
+                            <FontAwesomeIcon icon={icons[2]} className={styles.icon}/>
+                            {names[2]}
+                        </a>
+                        <a className="menu-item--small">
+                            <FontAwesomeIcon icon={icons[3]} className={styles.icon}/>
+                            {names[3]}
+                        </a>
+                        {selectionItems.map((value,index) => {
+                            <Link
+                                className="menu-item"
+                                to={value.target}
+                                spy={true}
+                                smooth={true}
+                                duration={1000}
+                                >
+                                    <FontAwesomeIcon icon={icons[index]} className={styles.icon}/>
+                                    {value.name}
+                            </Link>
+                        })}
                 </Menu>
             </div>
         </div>
