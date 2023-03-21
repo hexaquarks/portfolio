c04895e0917a9be046d2d7d87effa35aa83b6c95
hexaquarks <seseriusca@gmail.com>
2023-03-21 12:56:07 -0400

    Prettify Icons.tsx file

--- src/components/projects/Icons.tsx
+++ src/components/projects/Icons.tsx
@@ -1,35 +1,51 @@
-import javaIcon from '../../assets/java_icon.png'
-import javaFXIcon from '../../assets/javaFX_icon.png'
-import sceneBuilderIcon from '../../assets/sceneBuilder_icon.png'
-import cssIcon from '../../assets/css_icon.png'
-import javascriptIcon from '../../assets/javascript_icon_2.png'
-import reactIcon from '../../assets/react_icon.png'
-import cppIcon from '../../assets/cpp_icon.png'
-import sfmlIcon from '../../assets/sfml_icon.png'
-import particleFunGif from '../../assets/gifTry6.gif'
-import weatherGif from '../../assets/gifWeatherTry3.gif'
-import chessGif from '../../assets/ChessGamePromotionTraversal.gif'
-import researchGif from '../../assets/researchGif.gif'
-import portfolioEcaterinaGif from '../../assets/portfolioEcaterinaGif.gif'
-import knightIcon from '../../assets/knightIcon.png'
-import atomIcon from '../../assets/atomIcon.png'
-import weatherIcon from '../../assets/weatherIcon.png'
-import pythonIcon from '../../assets/pythonIcon.png'
-import proteinIcon from '../../assets/proteinIcon.png'
-import typescriptIcon from '../../assets/typescriptIcon.png'
-import portfolioIcon from '../../assets/portfolioIcon.png'
-import muiIcon from '../../assets/muiIcon.png'
+import javaIcon from '../../assets/java_icon.png';
+import javaFXIcon from '../../assets/javaFX_icon.png';
+import sceneBuilderIcon from '../../assets/sceneBuilder_icon.png';
+import cssIcon from '../../assets/css_icon.png';
+import javascriptIcon from '../../assets/javascript_icon_2.png';
+import reactIcon from '../../assets/react_icon.png';
+import cppIcon from '../../assets/cpp_icon.png';
+import sfmlIcon from '../../assets/sfml_icon.png';
+import particleFunGif from '../../assets/gifTry6.gif';
+import weatherGif from '../../assets/gifWeatherTry3.gif';
+import chessGif from '../../assets/ChessGamePromotionTraversal.gif';
+import researchGif from '../../assets/researchGif.gif';
+import portfolioEcaterinaGif from '../../assets/portfolioEcaterinaGif.gif';
+import knightIcon from '../../assets/knightIcon.png';
+import atomIcon from '../../assets/atomIcon.png';
+import weatherIcon from '../../assets/weatherIcon.png';
+import pythonIcon from '../../assets/pythonIcon.png';
+import proteinIcon from '../../assets/proteinIcon.png';
+import typescriptIcon from '../../assets/typescriptIcon.png';
+import portfolioIcon from '../../assets/portfolioIcon.png';
+import muiIcon from '../../assets/muiIcon.png';
 
-const gifs = [chessGif, particleFunGif,portfolioEcaterinaGif, researchGif, weatherGif];
-const placeholderIcons = [knightIcon, atomIcon, portfolioIcon, proteinIcon, weatherIcon];
+const gifs = [
+  chessGif,
+  particleFunGif,
+  portfolioEcaterinaGif,
+  researchGif,
+  weatherGif,
+];
+
+const placeholderIcons = [
+  knightIcon,
+  atomIcon,
+  portfolioIcon,
+  proteinIcon,
+  weatherIcon,
+];
 
 const techStackIcons = [
-    [cppIcon, 'C++', sfmlIcon, 'SFML'],
-    [javaIcon, 'Java', javaFXIcon, 'JavaFX', sceneBuilderIcon, 'ScenceBuilder', cssIcon, 'CSS'],
-    [typescriptIcon, 'Typescript', muiIcon, 'MaterialUI'],
-    [pythonIcon, 'Python'],
-    [javascriptIcon, 'Javascript', reactIcon, 'React', cssIcon, 'CSS']
+  [cppIcon, 'C++', sfmlIcon, 'SFML'],
+  [javaIcon, 'Java', javaFXIcon, 'JavaFX', sceneBuilderIcon, 'ScenceBuilder', cssIcon, 'CSS'],
+  [typescriptIcon, 'Typescript', muiIcon, 'MaterialUI'],
+  [pythonIcon, 'Python'],
+  [javascriptIcon, 'Javascript', reactIcon, 'React', cssIcon, 'CSS'],
+];
 
-]
-
-export { gifs, techStackIcons, placeholderIcons }
+export {
+  gifs,
+  techStackIcons,
+  placeholderIcons,
+};
